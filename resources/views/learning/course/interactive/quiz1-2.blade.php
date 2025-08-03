1f5589e6e8ea894d75f44d747739f574c29ecbb7
nicolasnch <nicolas.noel.ch@gmail.com>
2025-08-03 21:39:24 +0700

    feat(quiz): halaman blade dan js untuk kuis 2

---
+++ resources/views/learning/course/interactive/quiz1-2.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Quiz 1-1 | Mika Education</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo.png') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="show-asessment-dialog" content="true">
+    <meta name="progress-id" content="{{ session('progress_id') }}">
+    <meta name="user-id" content="{{ Auth::id() }}">
+    @vite('public/assets/css/style.css')
+    @vite('resources/js/quiz/quiz1/quiz1-2.js')
+</head>
+
+<body class="font-futura w-full min-h-screen flex flex-col relative text-blue31">
+    @include('includes.components.elearning.course.header')
+
+    <section class="w-full flex flex-grow items-start justify-start">
+        {{-- Quiz Section --}}
+        <div id="game-2" class="quiz-section h-[85vh] flex flex-col flex-grow">
+            <div class="w-full flex flex-col p-2">
+                <p class="p-2">Isilah lingkaran-lingkaran berikut dengan karakteristik Pra-Komunikasi yang sering
+                    ditunjukkan anak autistik.</p>
+            </div>
+            <div class="game-scene flex flex-1">
+                <div class="w-full flex-col justify-center">
+
+                    <!-- Title -->
+                    <div id="question"
+                        class="m-8 p-4 flex bg-blue31 rounded text-white font-bold text-center">
+                        Karakteristik Autisme
+                    </div>
+
+                    <!-- Input Box -->
+                    @php
+                        $number = ['1', '2', '3', '4'];
+                    @endphp
+                    <div class="flex flex-1 flex-col items-center justify-evenly">
+                        @foreach ($number as $i => $num)
+                            <div class="flex flex-1 items-center">
+                                <h1 class="m-4">{{ $num }}</h1>
+                                <div id=""
+                                    class="input-game rounded flex flex-1 items-center justify-center border border-blue31 text-sm text-blue-700"
+                                    <span>____</span> {{-- Placeholder for label --}}
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                {{-- Answer Section --}}
+                {{-- TODO: masukan jawaban ke database --}}
+                @php
+                    $answers = [
+                        'Kelemahan melakukan kemampuan perhatian bersama',
+                        'Anak kesulitan merespon dengan tepat ketika berkomunikasi dengan menggunakan gerak tubuh/gesture',
+                        'Melihat orang lain ketika berkomunikasi dengan lawan bicara (lebih banyak melihat ke arah lain)',
+                        'Sulit menggunakan dan memahami gestur dalam komunikasi',
+                        'Cenderung terbatas dalam komunikasi fungsional (untuk menyampaikan maksud/informasi dari diri ke orang lain)',
+                        'Orang lain menganggap anak kurang sopan',
+                    ];
+                @endphp
+                <div class="max-w-64 answer-card flex flex-col justify-evenly relative">
+                    @foreach ($answers as $answer)
+                        <div class="answer-game quiz-answer p-2">{{ $answer }}</div>
+                    @endforeach
+                </div>
+            </div>
+
+            {{-- Button --}}
+            <div class="x-5 py-3 w-1/3 flex justify-stretch self-end">
+                <button id="" type="button"
+                    class="w-full m-2 p-2 text-blue31 text-center border-2 border-blue31 rounded transition hover:-translate-y-1 hover:scale-105">Ulangi
+                    Kuis</button>
+                <a id="" href="{{ route('quiz.show', ['page' => 'quiz1-3']) }}"
+                    class="w-full m-2 p-2 text-white text-center bg-blue31 rounded transition hover:-translate-y-1 hover:scale-105">Kumpulkan</a>
+            </div>
+        </div>
+        @include('includes.components.elearning.course.section')
+    </section>
+
+    @include('includes.components.elearning.course.footer')
+</body>
+
+@include('includes.components.elearning.course.dialog.modal-asessment')
+
+</html>
