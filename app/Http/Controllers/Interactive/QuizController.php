71b022ac17c43d722b479b4a95c6d09deb1774d5
nicolasnch <nicolas.noel.ch@gmail.com>
2025-08-03 21:38:33 +0700

    fix(quiz): memperbaiki controller untuk navigasi kuis

--- app/Http/Controllers/Interactive/QuizController.php
+++ app/Http/Controllers/Interactive/QuizController.php
@@ -3,22 +3,16 @@
 namespace App\Http\Controllers\Interactive;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Support\Facades\View;
-use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    public function showQuiz(Request $page)
+    public function showQuiz($page)
     {
-        $viewPath = str_replace('.', '/', $page);
-
-        // Cek folder view apakah page yg diminta ada
         if (View::exists("learning.course.interactive.$page")) {
             return view("learning.course.interactive.$page");
         }
 
-        // If the view doesn't exist, show 404
-        abort(404, "Page not found: $page");
+        abort(404, 'Page not found');
     }
 }
