bb02c01ca52e11712ff96210c7fe97c9182b8773
nicolasnch <nicolas.noel.ch@gmail.com>
2025-08-03 14:47:27 +0700

    fix(quiz): memperbaiki controller untuk kuis interaktif

--- app/Http/Controllers/Interactive/QuizController.php
+++ app/Http/Controllers/Interactive/QuizController.php
@@ -1,32 +1,21 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Interactive;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    public function showQuiz(Request $request, $page)
+    public function showQuiz(Request $page)
     {
-        // You can explode or use it directly
         $viewPath = str_replace('.', '/', $page);
-        $quiz_id = $request->query('quiz_id'); // optionally passed in query string
-
-        // Simulate user progress_id from session (or wherever you store it)
-        $progress_id = session('progress_id'); // or Auth::user()->progress_id, etc.
-
-        // Cek apakah user sudah pernah mengikuti quiz
-        $sudahMengisi = false;
-        if ($quiz_id) {
-            $sudahMengisi = UserAsessment::where([
-                'progress_id' => $progress->progress_id,
-                'quiz_id' => $quiz_id,
-            ])->exists();
-        }
 
         // Cek folder view apakah page yg diminta ada
         if (View::exists("learning.course.interactive.$page")) {
-            return view("learning.course.interactive.$page", compact('sudahMengisi', 'quiz_id'));
+            return view("learning.course.interactive.$page");
         }
 
         // If the view doesn't exist, show 404
