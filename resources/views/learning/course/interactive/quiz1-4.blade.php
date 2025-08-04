fe2e2fa103838632301386d48104799a5180ad45
nicolasnch <nicolas.noel.ch@gmail.com>
2025-08-04 15:28:13 +0700

    feat(quiz): halaman blade dan js untuk kuis 4, minor fix halaman 3

--- resources/views/learning/course/interactive/quiz1-4.blade.php
+++ resources/views/learning/course/interactive/quiz1-4.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Quiz 1-3 | Mika Education</title>
+    <title>Quiz 1-4 | Mika Education</title>
     <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +12,7 @@
     <meta name="progress-id" content="{{ session('progress_id') }}">
     <meta name="user-id" content="{{ Auth::id() }}">
     @vite('public/assets/css/style.css')
-    @vite('resources/js/quiz/quiz1/quiz1-3.js')
+    @vite('resources/js/quiz/quiz1/quiz1-4.js')
 </head>
 
 <body class="font-futura w-full min-h-screen flex flex-col relative text-blue31">
@@ -29,15 +29,15 @@
                 <div class="w-full flex justify-center">
 
                     <!-- Title -->
-                    <div id="question" class="m-8 p-4 flex bg-blue31 rounded text-white font-bold text-center">
-                        Komunikasi Ekspresif
+                    <div id="question" class="m-8 p-4 flex bg-blue31 items-center rounded">
+                        <h2 class="-rotate-90 text-white font-bold">Komunikasi Reseptif</h2>
                     </div>
 
                     <!-- Input Box -->
-                    <div class="flex flex-grow flex-col items-center justify-evenly">
-                        @for ($i = 0; $i < 8; $i++)
+                    <div class="w-1/3 flex flex-col items-start justify-evenly">
+                        @for ($i = 0; $i < 3; $i++)
                             <div id=""
-                                class="js-input-game my-2 w-full flex flex-1 flex-col rounded items-center justify-center border border-blue31 text-sm text-blue-700"
+                                class="js-input-game my-2 w-full h-1/6 flex flex-col-reverse rounded items-center justify-center border border-blue31 text-sm text-blue-700"
                                 <span>____</span> {{-- Placeholder for label --}}
                             </div>
                         @endfor
@@ -47,15 +47,12 @@
                 {{-- TODO: masukan jawaban ke database --}}
                 @php
                     $answers = [
-                        'Membuat suara',
-                        'Menggerakkan jari dan tangan untuk memungut',
-                        'Menggunakan kata tunggal',
-                        'Menggunakan kata yang terdiri dari 2-3 kata',
-                        'Berbicara dalam kalimat',
-                        'Echolalia (mengulang kata atau kalimat yang diucapkan seseorang)',
+                        'Memahami pertanyaan yang baru didengarnya',
+                        'Mengajukan pertanyaan',
                         'Terpaku pada kualitas sensoris khas',
                         'Membuat komentar',
-                        'Melakukan percakapan/dialog',
+                        'Membuat komentar',
+                        'Memahami konsep abstrak seperti peribahasa, lawan kata, padanan kata dan majas',
                     ];
                 @endphp
                 <div class="js-answer-card max-w-64 flex flex-col justify-evenly relative">
@@ -70,7 +67,7 @@
                 <button id="" type="button"
                     class="w-full m-2 p-2 text-blue31 text-center border-2 border-blue31 rounded transition hover:-translate-y-1 hover:scale-105">Ulangi
                     Kuis</button>
-                <a id="" href="{{ route('quiz.show', ['quiz' => 'quiz1-4']) }}"
+                <a id="" href="{{ route('quiz.show', ['quiz' => 'quiz1-5']) }}"
                     class="w-full m-2 p-2 text-white text-center bg-blue31 rounded transition hover:-translate-y-1 hover:scale-105">Kumpulkan</a>
             </div>
         </div>
