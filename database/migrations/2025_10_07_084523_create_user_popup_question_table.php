011c96d5f32643ddcbf05b8bad2cae7d2b063056
nicolasnch <nicolas.noel.ch@gmail.com>
2025-10-09 16:44:36 +0700

    feat(popup): add popup question migration for database

---
+++ database/migrations/2025_10_07_084523_create_user_popup_question_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("user_popup_question", function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("module_id");
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("popup_question_id");
+            $table
+                ->foreign("popup_question_id")
+                ->references("id")
+                ->on("popup_question")
+                ->onDelete("cascade");
+            $table->boolean("is_passed");
+            $table->boolean("is_correct");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("user_popup_question");
+    }
+};
